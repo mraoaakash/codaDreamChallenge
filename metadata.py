90fdb4d70911cbcf6e496c8c168cf2c02c822099
Aakash Rao <mrao.aakash@gmail.com>
2023-01-24 10:13:25 +0530

    Created new Df with only required info

--- metadata.py
+++ metadata.py
@@ -10,7 +10,9 @@ basepath = "/home/chs.rintu/Documents/chs-lab-ws02/research-challenges/dream/cod
 
 patient_data = pd.read_csv(os.path.join(basepath, "CODA_TB_Clinical_Meta_Info.csv"))
 audio_data = pd.read_csv(os.path.join(basepath, "CODA_TB_Solicited_Meta_Info.csv"))
-# patient_data = patient_data['participant','tb_status']
-
-print(patient_data[['participant','tb_status']])
-print(audio_data['participant'])
+patient_data = patient_data[['participant','tb_status']]
+new_df = pd.merge(patient_data, audio_data, on='participant')
+new_df = new_df[['participant','tb_status','audio_file','audio_duration']]
+print(patient_data.head())
+print(audio_data.head())
+print(new_df.head())
