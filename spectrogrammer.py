bc54d4bd270ba77558625e617721b881d2de072d
Aakash Rao <mrao.aakash@gmail.com>
2023-01-24 10:57:04 +0530

    Added spectrogtram generator

--- spectrogrammer.py
+++ spectrogrammer.py
@@ -18,21 +18,22 @@ basepath = "/home/chs.rintu/Documents/chs-lab-ws02/research-challenges/dream/cod
 
 def spectrogram():
     for filename in os.listdir(basepath):
-        print(filename)
-        path = os.path.join(basepath, filename)
-        x, sr = librosa.load(path, sr=44100)
-        if librosa.get_duration(x, sr) == 0.5:
-            x = np.pad(x, (0, 22050), 'constant')
-        # print(librosa.get_duration(x, sr))
-        S = librosa.feature.melspectrogram(x, sr=sr, n_mels=128)
-        log_S = librosa.power_to_db(S, ref=np.max)
-        plt.figure(figsize=(5, 5))
-        librosa.display.specshow(log_S, sr=sr,fmax=8000)
-        plt.clim(-80,0)
-        plt.tight_layout()
-        # plt.show()
-        plt.savefig(f"{output_path}/{filename[:-4]}.png")
-        plt.clf()
+        if filename.endswith(".wav"):
+            print(filename)
+            path = os.path.join(basepath, filename)
+            x, sr = librosa.load(path, sr=44100)
+            if librosa.get_duration(x, sr) == 0.5:
+                x = np.pad(x, (0, 22050), 'constant')
+            # print(librosa.get_duration(x, sr))
+            S = librosa.feature.melspectrogram(x, sr=sr, n_mels=128)
+            log_S = librosa.power_to_db(S, ref=np.max)
+            plt.figure(figsize=(5, 5))
+            librosa.display.specshow(log_S, sr=sr,fmax=8000)
+            plt.clim(-80,0)
+            plt.tight_layout()
+            # plt.show()
+            plt.savefig(f"{output_path}/{filename[:-4]}.png")
+            plt.clf()
 
 if __name__ == "__main__":
     spectrogram()
