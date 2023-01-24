473ad7277dcc667fb2ab7a2332b8a905956db76d
Aakash Rao <mrao.aakash@gmail.com>
2023-01-24 09:25:42 +0530

    Added metadata filer

---
+++ metadata.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+
+
+basepath = "/home/chs.rintu/Documents/chs-lab-ws02/research-challenges/dream/coda-tb-22/Train/meta_data"
+
+patient_data = pd.read_csv(os.path.join(basepath, "CODA_TB_Clinical_Meta_Info.csv"))
+audio_data = pd.read_csv(os.path.join(basepath, "CODA_TB_Solicited_Meta_Info.csv"))
+
+print(patient_data.head())
+print(audio_data.head())
