9a370dd5d3b9099cba77069df528d418595a5148
Aakash Rao <mrao.aakash@gmail.com>
2023-01-24 16:08:12 +0530

    Added swin

--- swin.py
+++ swin.py
@@ -28,7 +28,7 @@ print("Number of Accelerators: ", strategy.num_replicas_in_sync)
 
 # Model
 IMAGE_SIZE = [224, 224] # Change this accordingly. 
-MODEL_PATH = "https://tfhub.dev/sayakpaul/swin_tiny_patch4_window7_224_fe" 
+MODEL_PATH = "https://tfhub.dev/sayakpaul/swin_tiny_patch4_window7_224" 
 
 # TPU
 BATCH_SIZE = 128  # on Colab/GPU, a higher batch size may throw OOM
