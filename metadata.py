a131f9f8d1180f25a325090ad4a0cb11340ceb70
Aakash Rao <mrao.aakash@gmail.com>
2023-01-24 11:07:45 +0530

    Added spectrogram code

--- metadata.py
+++ metadata.py
@@ -14,6 +14,23 @@ patient_data = patient_data[['participant','tb_status']]
 new_df = pd.merge(patient_data, audio_data, on='participant')
 new_df = new_df[['participant','tb_status','filename','sound_prediction_score']]
 
+
+# reading spectrograms to csv
+specdf = pd.DataFrame(columns=['spectrogram'])
+specpath = "/home/chs.rintu/Documents/chs-lab-ws02/research-challenges/dream/coda-tb-22/Train/raw_data/spect"
+for file in os.listdir(specpath):
+    if file.endswith(".png"):
+        specdf = specdf.append({'spectrogram': file}, ignore_index=True)
+specdf.to_csv(os.path.join(basepath, "spectrograms.csv"), index=False)
+
+# Sorting the datasets
+new_df = new_df.sort_values(by=['filename'])
+new_df = new_df.reset_index(drop=True)
+specdf = specdf.sort_values(by=['spectrogram'])
+specdf = specdf.reset_index(drop=True)
+new_df['spectrogram'] = specdf['spectrogram']
+
+
 # Individual datasets for positive and negative patients
 new_df_positive = new_df[new_df['tb_status'] == 1]
 new_df_negative = new_df[new_df['tb_status'] == 0]
@@ -43,6 +60,7 @@ print(train_negative.head())
 print(test_negative.head())
 print(train_df.head())
 print(test_df.head())
+
 
 # Saving the test and train datasets
 train_df.to_csv(os.path.join(basepath, "trainData.csv"), index=False)
