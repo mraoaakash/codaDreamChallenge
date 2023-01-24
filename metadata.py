cd68eab75ecc427a2d4a21c04ae700583206e601
Aakash Rao <mrao.aakash@gmail.com>
2023-01-24 09:32:54 +0530

    Added participant info

--- metadata.py
+++ metadata.py
@@ -9,7 +9,8 @@ import tensorflow as tf
 basepath = "/home/chs.rintu/Documents/chs-lab-ws02/research-challenges/dream/coda-tb-22/Train/meta_data"
 
 patient_data = pd.read_csv(os.path.join(basepath, "CODA_TB_Clinical_Meta_Info.csv"))
-audio_data = pd.read_csv(os.path.join(basepath, "CODA_TB_Solicited_Meta_Info.csv"))['participant','tb_status']
+audio_data = pd.read_csv(os.path.join(basepath, "CODA_TB_Solicited_Meta_Info.csv"))
+patient_data = patient_data['participant','tb_status']
 
 print(patient_data.head())
 print(audio_data.head())
